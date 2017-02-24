536a2b2e59ce48513bc49eb37bb78400796639bc
Bez Hermoso <bezalelhermoso@gmail.com>
2017-02-23 21:04:47 -0800

    Undo escaping on backspace

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 zsh-escape-backtick
 ===================
 
-## Important: If you happen to stumble on this don`t use it yet unless you want to lose ability to put in double-backticks
-
 Quickly insert escaped backticks (<code>\\\`</code>) when double-tapping <code>\`</code>.
 
 I sometimes need to add literal back-ticks in commit messages; this makes it a easier.
--- escape-backtick.zsh
+++ escape-backtick.zsh
@@ -1,14 +1,25 @@
 # Expands `` to \`
 function expand-double-backtick-to-escaped-backtick {
-  if [[ $LBUFFER = *\` ]]; then
+  if [[ $LBUFFER = *[^\\]\` ]]; then
     zle backward-delete-char
     LBUFFER+='\`'
+    bindkey '^?' undo-escaped-backtick-or-backward-delete-char
   else
     LBUFFER+='`'
   fi
 }
 
+function undo-escaped-backtick-or-backward-delete-char {
+  if [[ $LBUFFER = *\` ]]; then
+    zle backward-delete-char
+    zle backward-delete-char
+    LBUFFER+='`'
+  fi
+  bindkey '^?' backward-delete-char
+}
+
 zle -N expand-double-backtick-to-escaped-backtick
+zle -N undo-escaped-backtick-or-backward-delete-char
 
 bindkey "\`" expand-double-backtick-to-escaped-backtick
 
